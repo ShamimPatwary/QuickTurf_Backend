3845d0319d26cdb2373604c6668d2c75810eea83
Safwana Akhter <safwana220151@gmail.com>
2026-07-29 22:27:29 +0600

    turf_repository

--- app/repositories/turf_repository.py
+++ app/repositories/turf_repository.py
@@ -7,7 +7,8 @@ from app.repositories.base_repository import BaseRepository
 class TurfRepository(BaseRepository[Turf]):
     model = Turf
 
-   
+    def list_active(self) -> List[Turf]:
+        return self.db.query(Turf).filter(Turf.status == TurfStatus.ACTIVE).all()
 
     def add_image(self, turf_id: int, image_url: str) -> TurfImage:
         image = TurfImage(turf_id=turf_id, image_url=image_url)
