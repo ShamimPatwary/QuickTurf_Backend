0d9283eebd0c20e4c205ba32d76ff9f05d16613d
Shamim <shamimpatwary002@gmail.com>
2026-07-29 23:15:24 +0600

    packages

--- app/controllers/turf_admin_package_controller.py
+++ app/controllers/turf_admin_package_controller.py
@@ -6,3 +6,16 @@ from app.services.package_service import PackageService
 
 class TurfAdminPackageController(BaseController[PackageService]):
     service_class = PackageService
+
+    def create_package(self, turf_admin: TurfAdmin, data: PackageCreate) -> PackageOut:
+        return self.service.create_package(turf_admin, data)
+
+    def list_packages(self, turf_admin: TurfAdmin):
+        return self.service.list_packages(turf_admin)
+
+    def update_package(self, turf_admin: TurfAdmin, package_id: int, data: PackageUpdate) -> PackageOut:
+        return self.service.update_package(turf_admin, package_id, data)
+
+    def delete_package(self, turf_admin: TurfAdmin, package_id: int) -> dict:
+        self.service.delete_package(turf_admin, package_id)
+        return {"detail": "Package deleted"}
