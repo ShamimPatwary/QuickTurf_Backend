6b1e0763f0cc8621922dbe91c7c996d02776df63
Safwana Akhter <safwana220151@gmail.com>
2026-07-29 23:45:41 +0600

    repo

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -17,18 +17,7 @@ class BaseRepository(ABC, Generic[ModelType]):
     def get_by_id(self, id_: int) -> Optional[ModelType]:
         return self.db.query(self.model).filter(self.model.id == id_).first()
 
-  
+    def list_all(self) -> List[ModelType]:
+        return self.db.query(self.model).all()
 
-    def add(self, instance: ModelType) -> ModelType:
-        self.db.add(instance)
-        self.db.flush()
-        return instance
-
-    def delete(self, instance: ModelType) -> None:
-        self.db.delete(instance)
-
-    def commit(self) -> None:
-        self.db.commit()
-
-    def refresh(self, instance: ModelType) -> None:
-        self.db.refresh(instance)
+    
