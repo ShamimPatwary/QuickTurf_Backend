4cf40650ab811daa29a0ea420cc6e3cc9311938d
Shamim <shamimpatwary002@gmail.com>
2026-08-11 22:15:49 +0600

    changes

--- app/config.py
+++ app/config.py
@@ -11,6 +11,17 @@ class Settings(BaseSettings):
     WHATSAPP_API_URL: str = ""
     WHATSAPP_API_TOKEN: str = ""
 
+
+    SSLCOMMERZ_STORE_ID: str
+    SSLCOMMERZ_STORE_PASSWORD: str
+    SSLCOMMERZ_IS_SANDBOX: bool = True
+    SSLCOMMERZ_SUCCESS_URL: str 
+    SSLCOMMERZ_FAIL_URL: str
+    SSLCOMMERZ_CANCEL_URL: str
+    FRONTEND_URL: str
+
+
+
     class Config:
         env_file = ".env"
 
